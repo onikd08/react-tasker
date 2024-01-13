61583db82cb5e4efb130d5c54678c4b76c88f8d5
Anik Das <onikd08@gmail.com>
2024-01-13 07:33:42 +0200

    Replace star svg with react icon

--- src/components/Task/TaskRow.jsx
+++ src/components/Task/TaskRow.jsx
@@ -1,32 +1,17 @@
+import { FaStar } from "react-icons/fa";
 export default function TaskRow({ task }) {
   const { title, description, priority, isFavorite, tags } = task;
   return (
     <tr className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2">
-      <td>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="icon icon-tabler icon-tabler-star"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          strokeWidth="2"
-          stroke={isFavorite ? "yellow" : "white"}
-          fill={isFavorite ? "yellow" : ""}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
-        </svg>
-      </td>
+      <td>{isFavorite ? <FaStar color="yellow" /> : <FaStar />}</td>
       <td>{title}</td>
       <td>
         <div>{description}</div>
       </td>
       <td>
         <ul className="flex justify-center gap-1.5 flex-wrap">
-          {tags.map((tag, idx) => (
-            <li key={idx}>
+          {tags.map((tag) => (
+            <li key={tag}>
               <span className="inline-block h-5 whitespace-nowrap rounded-[45px] bg-[#00D991A1] px-2.5 text-sm capitalize text-[#F4F5F6]">
                 {tag}
               </span>
